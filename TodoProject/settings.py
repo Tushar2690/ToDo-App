aa525675cffdb12aee4c0a02dd46867293d4bc4d
Tushar Ranjan Pal <tusharranjanpal2692001@gmail.com>
2023-10-10 20:51:17 +0530

    change

--- TodoProject/settings.py
+++ TodoProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zn*pia4o^5$*%1=yd(r($vgg(tx_lc+gs)%%64^1!(qi0^o#az'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
